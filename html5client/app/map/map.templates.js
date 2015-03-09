f1730b6fdcd32cc17140995ef29605adb5115174
mba@gmx.ch <Drag0ner>
2015-03-09 19:44:22 +0100

    asdf

--- html5client/app/map/map.templates.js
+++ html5client/app/map/map.templates.js
@@ -14,14 +14,12 @@ bernApp.MapTemplates = (function () {
             '<h1><%- data.name %></h1>' +
             '<img class="mapPanelImg" src="../poiData/<%- data.imageSrc %>">' +
             '<p><%- data.content %></p>' +
-            '<p />' +
-
             '<a href="<%- data.link %>" rel="external" data-ajax="false" class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-action">Visit homepage</a>' +
             '<a href="#" data-ajax="false" onclick="bernApp.Navigation.getDirections(<%- json %>);"  class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-navigation">Get directions</a>' +
             '<a href="#" data-ajax="false" onclick="bernApp.Navigation.addPOI(<%- json %>);"  class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-plus">Add to agenda</a>' +
-
             '</div>'
         ),
+
         categoryDropdownTemplate: _.template(
             '<select name="categoryDropdownSelect" data-native-menu="false" id="categoryDropdownSelect">' +
             '<option value="choose-one" data-placeholder="true">Filter</option>' +
