4e4ca2f539f3eb55e411e7cb6144788ac874da1f
binzm2 <Drag0ner>
2015-03-11 16:54:50 +0100

    asdf

--- html5client/app/map/map.templates.js
+++ html5client/app/map/map.templates.js
@@ -17,6 +17,7 @@ bernApp.MapTemplates = (function () {
             '<a href="<%- data.link %>" rel="external" data-ajax="false" class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-action">Visit homepage</a><br>' +
             //'<a href="#" onClick="navigator.app.loadUrl(\'<%- data.link %>\', { openExternal:true });return false;" class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-action">Visit homepage</a><br>' +
             '<a href="tel:<%- data.telNbr %>" class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-phone">Call</a><br>' +
+            '<a href="mailto:<%- data.email %>" class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-mail">Send mail</a><br>' +
             '<a href="#" data-ajax="false" onclick="bernApp.Navigation.getDirections(<%- json %>);"  class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-navigation">Get directions</a><br>' +
             '<a href="#" data-ajax="false" onclick="bernApp.Navigation.addPOI(<%- json %>);"  class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-icon-left ui-icon-plus">Add to agenda</a><br>' +
             '</div>'
